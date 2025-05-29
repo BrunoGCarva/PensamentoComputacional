0bd870f2afcb7f0ddfa347b307ddeee681280c5b
Bruno Gabriel Carvalho <112557823+BrunoGCarva@users.noreply.github.com>
2025-05-28 21:47:49 -0300

    2805

--- main.py
+++ main.py
@@ -1,24 +1,11 @@
-from pydoc import text
-import tkinter as tk
-from tkinter import font
-def mostrar_opcao(botao):
-    rotulo.config(text=f"Escolheu: {botao}")
-    
-    if opcao1.get() and opcao2.get() and opcao3.get():
-        if botao == "Saúde":
-            opcao1.set(False)
-            texto = "Dinheiro"
-            
-janela = tk.Tk()
-janela.title("Exemplo Radiobutton")
-janela.geometry("250x150")
-opcao1 = tk.BooleanVar()
-opcao2 = tk.BooleanVar()
-opcao3 = tk.BooleanVar()
-
-tk.Radiobutton(janela, text="Tempo", variable=opcao1, value=True, command=lambda:mostrar_opcao("Tempo")).pack()
-tk.Radiobutton(janela, text="Dinhero", variable=opcao2, value=True, command=lambda: mostrar_opcao("Dinheiro")).pack()
-tk.Radiobutton(janela, text="Saúde", variable=opcao3, value=True, command=lambda: mostrar_opcao("Saúde")).pack()
-rotulo = tk.Label(janela, text="Escolheu:")
-rotulo.pack(pady=10)
-janela.mainloop()
+import re
+def validar_cpf(cpf):
+    cpf = re.sub(r'[^0-9]','', cpf)
+    if len(cpf) != 11:
+        return False
+    if cpf == cpf[0] * 11:
+        return False
+    padrao_formatado = r'^\d{3}\.\d{3}\.\d{3}-\d{2}$'
+    return True
+cpf_formatado ='123.456.789-09'
+print(f'CPF {cpf_formatado}: {'Válido' if re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$', cpf_formatado)else 'Formato inválido'}')
